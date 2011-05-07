4ff32ba284de00f38e1837a6e4ddf3b7e788be4c
chester.nht.1986@gmail.com <chester.nht.1986@gmail.com@606161a8-e195-cb49-43db-f180a112378e>
2011-05-07 06:05:16 +0000

    Implement Exam screen, to load exam from selected level, and be able to navigate between questions

--- trunk/src/vn/tonnguyen/sathach/ExamScreen.java
+++ trunk/src/vn/tonnguyen/sathach/ExamScreen.java
@@ -1,16 +1,28 @@
 package vn.tonnguyen.sathach;
 
+import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Random;
 
 import vn.tonnguyen.sathach.bean.ExamFormat;
+import vn.tonnguyen.sathach.bean.Level;
 import vn.tonnguyen.sathach.bean.Question;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ExamScreen extends BaseActivity {
+	private MyApplication application;
+	private int currentQuestionIndex; // to mark the index of the current displaying question
+	private Question[] examQuestions; // hold the list of random questions
+	private WebView full; // a WebView, to display question image
 	
 	/** Called when the activity is first created. */
 	@Override
@@ -18,50 +30,152 @@ public class ExamScreen extends BaseActivity {
 
 		super.onCreate(savedInstanceState);
 		
-		MyApplication application = (MyApplication)getApplicationContext();
-		Hashtable<Integer, Question> examQuestions = new Hashtable<Integer, Question>();
-		Random randomGenerator = new Random();
+		application = (MyApplication)getApplicationContext();
+		if(application.getSelectedLevelIndex() < 0) {
+			Toast.makeText(application, application.getString(R.string.error_pleaseSelect_Level), Toast.LENGTH_LONG)
+				.show();
+			return;
+		}
+		
+		setContentView(R.layout.exam);
+		
+		// generate exam
+		examQuestions = generateRandomQuestion(application.getLevels().get(application.getSelectedLevelIndex()));
+
+		full = (WebView)findViewById(R.id.webView);
+        //Make sure links in the webview is handled by the webview and not sent to a full browser
+		full.setWebViewClient(new WebViewClient());
+		full.getSettings().setSupportZoom(true);       //Zoom Control on web (You don't need this
+        //if ROM supports Multi-Touch     
+		full.getSettings().setBuiltInZoomControls(true);
+		//full.getSettings().setLoadWithOverviewMode(true);
+		full.getSettings().setUseWideViewPort(true);
+		full.setInitialScale(75); // so the phone can display the whole image on screen. This value should be persist if user change the zoom level
+		// so they dont have to change the zoom level every time they view a question
+		
+		// bind click event for next and previous buttons
+		((Button)findViewById(R.id.buttonFirst)).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				gotoFirst();
+			}
+		});
+		((Button)findViewById(R.id.buttonLast)).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				gotoLast();
+			}
+		});
+		((Button)findViewById(R.id.buttonNext)).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				next();
+			}
+		});
+		((Button)findViewById(R.id.buttonPrevious)).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				previous();
+			}
+		});
+		
+		// display the first question
+		currentQuestionIndex = 0;
+		showQuestion(currentQuestionIndex);
+	}
+	
+	/***
+	 * Listen on back button pressed, to navigate through questions instead, or asking user to exit if they are viewing the first question 
+	 */
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event)  {
+	    if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+	        if(currentQuestionIndex == 0) {
+	        	// asking user to exit if they are viewing the first question
+	        	onExit();
+	        } else {
+	        	previous();
+	        }
+	        return true;
+	    }
+	    return super.onKeyDown(keyCode, event);
+	}
+	
+	private void onExit() {
+		// confirm to exit exam
+    	new AlertDialog.Builder(ExamScreen.this)
+		.setIcon(android.R.drawable.ic_dialog_info)
+		.setTitle(R.string.exam_exit_confirm_title)
+		.setMessage(R.string.exam_exit_confirm_message)
+		.setPositiveButton(R.string.exam_exit_confirm_ok,
+				new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						finish();
+					}
+				})
+		.setNegativeButton(R.string.exam_exit_confirm_cancel,
+				new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog,
+							int which) {
+						// do nothing
+					}
+				}).show();
+	}
+	
+	private void gotoFirst() {
+		currentQuestionIndex = 0;
+		showQuestion(currentQuestionIndex);
+	}
+	
+	private void gotoLast() {
+		currentQuestionIndex = examQuestions.length - 1;
+		showQuestion(currentQuestionIndex);
+	}
+	
+	private void next() {
+		showQuestion(++currentQuestionIndex);
+	}
+	
+	private void previous() {
+		showQuestion(--currentQuestionIndex);
+	}
+	
+	/**
+	 * Display the question to UI
+	 * @param questionIndex index of the question, to get from examQuestions
+	 */
+	private void showQuestion(int questionIndex) {
+		/* Create a new Html that contains the full-screen image */
+		Log.d("Displaying question", "Index: " + questionIndex + " - " + examQuestions[questionIndex].toString());
+		String html = "<html><img src=\"" + examQuestions[questionIndex].getPictureName() + "\"></html>";
+		/* Finally, display the content using WebView */
+		full.loadDataWithBaseURL("file:///" + MyApplication.APPLICATION_DATA_PATH, html, "text/html", "utf-8", "");
+	}
+	
+	private Question[] generateRandomQuestion(Level selectedLevel) {
+		// generate exam:
+		Hashtable<Integer, Question> examQuestions = new Hashtable<Integer, Question>(); // hold the list of random questions
+		Random randomGenerator = new Random(); // used to generate random int
+		ArrayList<ExamFormat> examFormats = selectedLevel.getExamsFormat();
 		int questionIndex;
-		// generate B1 exam:
-		for (ExamFormat examformat : application.getLevels().get(0).getExamsFormat()) {
+		ExamFormat examformat;
+		int formatSize = examFormats.size();
+		for (int x = 0; x < formatSize; x++) { // for loop will be faster that for each loop with ArrayLis
+			examformat = examFormats.get(x);
 			// loop through NumberOfQuestion to get enough questions
 			for (int i = 1; i <= examformat.getNumberOfQuestion(); i++) {
-				// get random question index, and make sure it does not exist in
-				// the list of questions
+				// get random question index, and make sure it does not exist in the list of questions
 				do {
-					questionIndex = getRandomInteger(examformat.getFrom(),
-							examformat.getTo(), randomGenerator);
+					questionIndex = getRandomInteger(examformat.getFrom(), examformat.getTo(), randomGenerator);
 				} while (examQuestions.containsKey(questionIndex));
 				// Now we have the question that was not existed in question list
 				Log.v("questionIndex", String.valueOf(questionIndex));
 				examQuestions.put(questionIndex, application.getQuestions().get(questionIndex));
 			}
 		}
-		
-		String text = "";
-		for(Integer key : examQuestions.keySet()) {
-			text += (application.getQuestions().get(key).toString() + "\n");
-		}
-
-		/* Using WebView to display the full-screen image */
-		
-		setContentView(R.layout.exam);
-		WebView full = (WebView)findViewById(R.id.webView);
-        //Make sure links in the webview is handled by the webview and not sent to a full browser
-		full.setWebViewClient(new WebViewClient());
-		
-		full.getSettings().setSupportZoom(true);       //Zoom Control on web (You don't need this
-        //if ROM supports Multi-Touch     
-		full.getSettings().setBuiltInZoomControls(true);
-		//full.getSettings().setLoadWithOverviewMode(true);
-		full.getSettings().setUseWideViewPort(true);
-		full.setInitialScale(75);
-
-		/* Create a new Html that contains the full-screen image */
-		String html = (text + "<html><img src=\"017.jpg\"></html>");
-		/* Finally, display the content using WebView */
-		full.loadDataWithBaseURL("file:///" + MyApplication.APPLICATION_DATA_PATH, html, "text/html", "utf-8",
-				"");
+		return examQuestions.values().toArray(new Question[examQuestions.size()]);
 	}
 	
 	private static int getRandomInteger(int aStart, int aEnd, Random aRandom) {
--- trunk/src/vn/tonnguyen/sathach/Home.java
+++ trunk/src/vn/tonnguyen/sathach/Home.java
@@ -1,21 +1,42 @@
 package vn.tonnguyen.sathach;
 
+import java.util.ArrayList;
+
+import vn.tonnguyen.sathach.bean.Level;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Home extends BaseActivity {
 	private Button newExamButton;
 	private Button exitButton;
+	private ArrayList<Level> levels;
+	private MyApplication application;
 	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
 		setContentView(R.layout.home);
 
+		application = (MyApplication)getApplicationContext();
+		// get list of menu item, which will be shown to ask user to select a level to create new exam
+		levels = application.getLevels();
+		if(levels == null || levels.size() < 1) {
+			Toast.makeText(application, application.getString(R.string.error_data_corrupted), Toast.LENGTH_LONG)
+				.show();
+			return;
+		}
+		final String[] menuItems = new String[levels.size()];
+		for(int i = 0; i < levels.size(); i++) {
+			menuItems[i] = levels.get(i).getName();
+		}
+		
 		// Capture our button from layout
 		newExamButton = (Button)findViewById(R.id.button_new_game);
 	    // Register the onClick listener with the implementation above
@@ -23,7 +44,32 @@ public class Home extends BaseActivity {
 			
 			@Override
 			public void onClick(View v) {
-				startActivity(new Intent((MyApplication)getApplicationContext(), ExamScreen.class));
+				AlertDialog.Builder selectLevelDialog = new AlertDialog.Builder(Home.this);
+				selectLevelDialog.setTitle(R.string.home_SelectlLevel_Title);
+				selectLevelDialog.setSingleChoiceItems(menuItems, 0, new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int whichButton) {
+						// onClick Action
+						// the level list has been ordered by index, so whichButton will be the selected index
+						application.setSelectedLevelIndex(whichButton);
+					}
+				}).setPositiveButton(R.string.ok_button, new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int whichButton) {
+						// on Ok button action
+						Log.v("Selected level index to create new exam", String.valueOf(application.getSelectedLevelIndex()));
+						if(application.getSelectedLevelIndex() >= 0) {
+							startActivity(new Intent((MyApplication)getApplicationContext(), ExamScreen.class));
+						} else {
+							Toast.makeText(application, application.getString(R.string.error_pleaseSelect_Level), Toast.LENGTH_LONG)
+								.show();
+						}
+					}
+				}).setNegativeButton(R.string.cancel_button, new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int whichButton) {
+						// on cancel button action
+						dialog.cancel();
+					}
+				});
+				selectLevelDialog.show();
 			}
 		});
 
