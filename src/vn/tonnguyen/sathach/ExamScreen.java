f23e2e19872722c9a6d47238f99cd1400afeeca7
chester.nht.1986 <chester.nht.1986@gmail.com>
2011-05-08 06:36:20 +0000

    Add exit menu for exam screen Implemented save and restore state for exam screen. Now session will persist when rotation changed

--- src/vn/tonnguyen/sathach/ExamScreen.java
+++ src/vn/tonnguyen/sathach/ExamScreen.java
@@ -1,6 +1,8 @@
 package vn.tonnguyen.sathach;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Hashtable;
 import java.util.Random;
 
@@ -12,13 +14,18 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class ExamScreen extends BaseActivity {
-	private MyApplication application;
+	private MyApplication context;
 	private int currentQuestionIndex; // to mark the index of the current displaying question
 	private Question[] examQuestions; // hold the list of random questions
 	private WebView webView; // a WebView, to display question image
@@ -29,10 +36,10 @@ public class ExamScreen extends BaseActivity {
 
 		super.onCreate(savedInstanceState);
 		
-		application = (MyApplication)getApplicationContext();
-		int levelIndex = application.getRecentlyLevel();
+		context = (MyApplication)getApplicationContext();
+		int levelIndex = context.getRecentlyLevel();
 		if(levelIndex < 0) {
-			Toast.makeText(application, application.getString(R.string.error_pleaseSelect_Level), Toast.LENGTH_LONG)
+			Toast.makeText(context, context.getString(R.string.error_pleaseSelect_Level), Toast.LENGTH_LONG)
 				.show();
 			return;
 		}
@@ -46,7 +53,7 @@ public class ExamScreen extends BaseActivity {
 		webView.getSettings().setBuiltInZoomControls(true);
 		//full.getSettings().setLoadWithOverviewMode(true);
 		webView.getSettings().setUseWideViewPort(true);
-		webView.setInitialScale(application.getRecentlyZoom()); // so the phone can display the whole image on screen. 
+		webView.setInitialScale(context.getRecentlyZoom()); // so the phone can display the whole image on screen. 
 		// This value should be persist if user change the zoom level
 		// so they dont have to change the zoom level every time they view a question
 		
@@ -75,31 +82,89 @@ public class ExamScreen extends BaseActivity {
 				previous();
 			}
 		});
+		((RadioButton)findViewById(R.id.radioAnswer1)).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				updateChoice(1);
+			}
+		});
+		((RadioButton)findViewById(R.id.radioAnswer2)).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				updateChoice(2);
+			}
+		});
+		((RadioButton)findViewById(R.id.radioAnswer3)).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				updateChoice(3);
+			}
+		});
+		((RadioButton)findViewById(R.id.radioAnswer4)).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				updateChoice(4);
+			}
+		});
 		
 		// TODO Check to restore session
-		
-		// generate exam
-		examQuestions = generateRandomQuestion(application.getLevels().get(levelIndex));
-		
-		// display the first question
-		currentQuestionIndex = 0;
-		showQuestion(currentQuestionIndex);
+		if(savedInstanceState != null) { // question will be retrieve back and display in onRestoreInstanceState
+			//Log.d("Get question from saved state", "onCreate");
+			//examQuestions = (Question[])savedInstanceState.getSerializable("CurrentQuestions");
+		} else {
+			// generate exam
+			examQuestions = generateRandomQuestion(context.getLevels().get(levelIndex));
+			// display the first question
+			currentQuestionIndex = 0;
+			showQuestion(currentQuestionIndex);
+		}
+	}
+	
+	/**
+	 * Create option menu
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.exam_screen_menu, menu);
+	    return true;
+	}
+	
+	/**
+	 * Process when user click on option menu
+	 */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle item selection
+	    switch (item.getItemId()) {
+	    case R.id.end_exam:
+	        onExit();
+	        return true;
+	    default:
+	        return super.onOptionsItemSelected(item);
+	    }
 	}
 	
 	@Override
 	protected void onSaveInstanceState(Bundle state) {
-		super.onSaveInstanceState(state);
 		Log.d("ExamScreen", "onSaveInstanceState " + state.toString());
 		// save the current session, so next time when user come back, we will load it
+		state.putSerializable("CurrentQuestions", examQuestions);
+		state.putInt("currentQuestionIndex", currentQuestionIndex);
 		saveSession();
+		super.onSaveInstanceState(state);
 	}
 	
 	@Override
 	protected void onRestoreInstanceState(Bundle state) {
-		super.onRestoreInstanceState(state);
 		Log.d("ExamScreen", "onRestoreInstanceState " + state.toString());
+		Log.d("Get question from saved state", "onRestore");
 		// restore the last session of user
+		examQuestions = (Question[])state.getSerializable("CurrentQuestions");
+		currentQuestionIndex = state.getInt("currentQuestionIndex");
+		showQuestion(currentQuestionIndex); // display the last viewed question
 		restoreSession();
+		super.onRestoreInstanceState(state);
 	}
 	
 	/**
@@ -143,6 +208,8 @@ public class ExamScreen extends BaseActivity {
 				new DialogInterface.OnClickListener() {
 					@Override
 					public void onClick(DialogInterface dialog, int which) {
+						// show result
+						showResult();
 						finish();
 					}
 				})
@@ -154,6 +221,19 @@ public class ExamScreen extends BaseActivity {
 						// do nothing
 					}
 				}).show();
+	}
+	
+	private void showResult() {
+		// get the number of right answers
+		int rightChoice = 0;
+		for(Question question : examQuestions) {
+			if(question.getAnswer() == question.getUserChoice()) {
+				rightChoice++;
+			}
+		}
+		
+		Toast toast = Toast.makeText(context, rightChoice + "/" + examQuestions.length, Toast.LENGTH_LONG);
+		toast.show();
 	}
 	
 	private void gotoFirst() {
@@ -181,18 +261,49 @@ public class ExamScreen extends BaseActivity {
 	}
 	
 	/**
+	 * Update user choice for current viewing question
+	 * @param choice choice that user has chosen
+	 */
+	private void updateChoice(int choice) {
+		Question currentQuestion = examQuestions[currentQuestionIndex];
+		Log.d("User answer", "Question " + currentQuestion.getPictureName() + 
+								" - User choice: " + choice + 
+								" - Answer: " + currentQuestion.getAnswer());
+		currentQuestion.setUserChoice(choice);
+	}
+	
+	/**
 	 * Display the question to UI
 	 * @param questionIndex index of the question, to get from examQuestions
 	 */
 	private void showQuestion(int questionIndex) {
+		RadioGroup radioGroup = (RadioGroup)findViewById(R.id.radioGroup); 
+		Question questionToShow = examQuestions[questionIndex];
 		/* Create a new Html that contains the full-screen image */
-		Log.d("Displaying question", "Index: " + questionIndex + " - " + examQuestions[questionIndex].toString());
-		String html = "<html><img src=\"" + examQuestions[questionIndex].getPictureName() + "\"></html>";
+		Log.d("Displaying question", "Index: " + questionIndex + " - " + questionToShow.toString());
+		String html = "<html><img src=\"" + questionToShow.getPictureName() + "\"></html>";
 		/* Finally, display the content using WebView */
 		int scale = (int)(100 * webView.getScale());
-		application.setRecentlyZoom(scale);
+		context.setRecentlyZoom(scale);
 		webView.loadDataWithBaseURL("file:///" + MyApplication.APPLICATION_DATA_PATH, html, "text/html", "utf-8", "");
 		webView.setInitialScale(scale);
+		
+		// disable answer-radio-button that won't be used:
+		radioGroup.clearCheck(); // clear answer
+		Log.d("radioGroup.getChildCount()", String.valueOf(radioGroup.getChildCount()));
+		int childCount = radioGroup.getChildCount();
+		// disable all radio button in this group
+		for(int i = 0; i < childCount; i++) {
+			radioGroup.getChildAt(i).setEnabled(false);
+		}
+		// then enable only if answer is available
+		for(int i = 0; i < questionToShow.getNumberOfAnswers(); i++) {
+			radioGroup.getChildAt(i).setEnabled(true);
+		}
+		// if user has chosen a choice for this question, lets select it
+		if(questionToShow.getUserChoice() > 0) {
+			((RadioButton)radioGroup.getChildAt(questionToShow.getUserChoice() - 1)).setChecked(true);
+		}
 	}
 	
 	private Question[] generateRandomQuestion(Level selectedLevel) {
@@ -213,10 +324,13 @@ public class ExamScreen extends BaseActivity {
 				} while (examQuestions.containsKey(questionIndex));
 				// Now we have the question that was not existed in question list
 				Log.v("questionIndex", String.valueOf(questionIndex));
-				examQuestions.put(questionIndex, application.getQuestions().get(questionIndex));
+				examQuestions.put(questionIndex, context.getQuestions().get(questionIndex));
 			}
 		}
-		return examQuestions.values().toArray(new Question[examQuestions.size()]);
+		// sort the question list as random
+		Question[] questionArr = examQuestions.values().toArray(new Question[examQuestions.size()]);
+		Collections.shuffle(Arrays.asList(questionArr));
+		return questionArr;
 	}
 	
 	private static int getRandomInteger(int aStart, int aEnd, Random aRandom) {
