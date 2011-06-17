3ba7dc4d48731cef75ed9a2b73f1cdee6be16956
chester.nht.1986@gmail.com <chester.nht.1986@gmail.com@606161a8-e195-cb49-43db-f180a112378e>
2011-06-17 03:57:11 +0000

    - updated GPL license Terms template

--- trunk/src/com/tonnguyen/sathach/QuestionDbAdapter.java
+++ trunk/src/com/tonnguyen/sathach/QuestionDbAdapter.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2011  Nguyen Hoang Ton, a.k.a Ton Nguyen
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 package com.tonnguyen.sathach;
 
 import android.content.ContentValues;
@@ -10,6 +28,10 @@ import android.util.Log;
 
 import com.tonnguyen.sathach.bean.UserResult;
 
+/**
+ * @author Ton Nguyen
+ *
+ */
 public class QuestionDbAdapter {
     public static final String KEY_RESULT_ID = "QuestionName";
     public static final String KEY_RESULT_NUM_OF_APPEAR = "NumOfAppear";
