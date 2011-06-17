88cfb04e932d0dcdb809d4f28008650d94caa0d6
chester.nht.1986 <chester.nht.1986@gmail.com>
2011-06-17 03:57:11 +0000

    - updated GPL license Terms template

--- src/com/tonnguyen/sathach/ExamActivity.java
+++ src/com/tonnguyen/sathach/ExamActivity.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2011  Nguyen Hoang Ton, a.k.a Ton Nguyen
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package com.tonnguyen.sathach;
 
 import java.io.IOException;
@@ -35,6 +52,10 @@ import com.tonnguyen.sathach.bean.QuestionReviewSession;
 import com.tonnguyen.sathach.bean.QuestionState;
 import com.tonnguyen.sathach.bean.Session;
 
+/**
+ * @author Ton Nguyen
+ *
+ */
 public class ExamActivity extends BaseActivity {
 	private int currentQuestionIndex; // to mark the index of the current displaying question
 	private long remainingTime;// = EXAM_TIME; // the remaining exam time that user has(they have 20 minutes for each exam)
--- src/com/tonnguyen/sathach/StartupActivity.java
+++ src/com/tonnguyen/sathach/StartupActivity.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2011  Nguyen Hoang Ton, a.k.a Ton Nguyen
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 package com.tonnguyen.sathach;
 
 import java.io.File;
@@ -21,6 +39,10 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+/**
+ * @author Ton Nguyen
+ *
+ */
 public class StartupActivity extends BaseActivity {
 	public static final int REQUEST_CODE_START_EXAM = 1;
 	public static final int REQUEST_CODE_VIEW_RESULT = 2;
